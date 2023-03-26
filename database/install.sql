fe5ada8867638c21754cd2c75632e26cfb0102c5
Sébastien Thomas-Girard <arieseb@gmail.com>
2023-03-26 19:58:52 +0200

    Add database initialization file

---
+++ database/install.sql
@@ -0,0 +1,3 @@
+INSERT INTO restaurant
+(name, max_guests, business_days, opening_hour, closing_hour, current_guests)
+VALUES ("Le Quai Antique", 32, "a:5:{i:0;s:5:"Mardi";i:1;s:8:"Mercredi";i:2;s:5:"Jeudi";i:3;s:8:"Vendredi";i:4;s:6:"Samedi";}", "12:00", "15:00", 0)
